0da9e9bb0b71fc281c6defa9bfa92018b8563840
Tanish <tanishazad03@gmail.com>
2025-06-24 22:51:08 +0530

    remove desktop target (#1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,6 @@ plugins {
 }
 
 kotlin {
-    jvm("desktop")
-
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         moduleName = "composeApp"
@@ -32,42 +30,21 @@ kotlin {
         binaries.executable()
     }
 
-    sourceSets {
-        val desktopMain by getting
-        
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.materialIconsExtended)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+    sourceSets.commonMain.dependencies {
+        implementation(compose.runtime)
+        implementation(compose.foundation)
+        implementation(compose.material3)
+        implementation(compose.ui)
+        implementation(compose.materialIconsExtended)
+        implementation(compose.components.resources)
+        implementation(compose.components.uiToolingPreview)
 
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation(libs.androidx.navigation)
+        implementation(libs.androidx.lifecycle.viewmodel)
+        implementation(libs.androidx.lifecycle.runtime.compose)
+        implementation(libs.androidx.navigation)
 
-            implementation(libs.kotlinx.serialization.json)
-            implementation(libs.kotlinx.multik.core)
-            implementation(libs.kotlinx.multik.default)
-        }
-
-        desktopMain.dependencies {
-            implementation(compose.desktop.currentOs)
-            implementation(libs.kotlinx.coroutines.swing)
-        }
-    }
-}
-
-compose.desktop {
-    application {
-        mainClass = "io.github.taz03.matrix.calculator.MainKt"
-
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "io.github.taz03.matrix.calculator"
-            packageVersion = "1.0.0"
-        }
+        implementation(libs.kotlinx.serialization.json)
+        implementation(libs.kotlinx.multik.core)
+        implementation(libs.kotlinx.multik.default)
     }
 }
